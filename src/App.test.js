7c370dbab3b1142211360a571c8acb83ed5c9229
Oldnickel <nyamaocollins@gmail.com>
2024-02-09 23:53:52 +0300

    CA-7 : added sample tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import TestDummyComponent from './components/test-dummy/TestDummyComponent';
+import CounterTest from './components/Tests/CounterTest';
 
 
 test('renders learn react link ', () => {
   render(<TestDummyComponent />);
   const linkElement = screen.getByText(/Test/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders counter with initial value', () => {
+  const { getByText } = render(<CounterTest />);
+  const counterElement = getByText('Counter: 0');
+  expect(counterElement).toBeInTheDocument();
+});
+
+test('increments counter when the button is clicked', () => {
+  const { getByText } = render(<CounterTest />);
+  const counterElement = getByText('Counter: 0');
+  const buttonElement = getByText('Increment');
+
+  fireEvent.click(buttonElement);
+
+  expect(counterElement).toHaveTextContent('Counter: 1');
 });
---
+++ src/components/Tests/CounterTest.jsx
@@ -0,0 +1,19 @@
+// Counter.js
+import React, { useState } from "react";
+
+function CounterTest() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <h2>Counter: {count}</h2>
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
+}
+
+export default CounterTest;
